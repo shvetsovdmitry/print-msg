3f0ff5c69d7345c666742635aebfe50526f3e3a0
GrobIvanovich <killmaster00431@gmail.com>
2019-02-25 17:02:44 +0500

    Fixed bugs.

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="print_msg",
-    version="0.9",
+    version="0.9.3",
     author="Shvetsov Dmitry",
     author_email="shvetsovdmitry1996@gmail.com",
     description="Print colored status messages.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/shvetsovdmitry/print-msg",
+    py_modules=['print_msg'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
