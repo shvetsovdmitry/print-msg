b531e564db8c112cec3a0afc659295f04a9d2e22
GrobIvanovich <killmaster00431@gmail.com>
2019-02-25 17:02:04 +0500

    Added 'end' parameter.

--- print_msg.py
+++ print_msg.py
@@ -1,18 +1,18 @@
 from termcolor import colored
 
 
-def print_msg(status: str, msg: str):
+def print_msg(status: str, msg: str, end='\n'):
     """Print status messages.
     status = 'ok', 'status' or 'error'."""
     if status == 'ok':
         print('[--{0}--] - {1}'.format(colored('OK', 'green'),
-                                       colored(msg, 'yellow')))
+                                       colored(msg, 'yellow')), end=end)
     elif status == 'error':
         print('[{0}] - {1}'.format(colored('ERROR!', 'red'),
-                                   colored(msg, 'pink')))
+                                   colored(msg, 'magenta')), end=end)
     elif status == 'status':
         print('[{0}] - {1}'.format(colored('STATUS', 'yellow'),
-                                   colored(msg, 'green')))
+                                   colored(msg, 'green')), end=end)
     elif status == 'time':
         print('[-{0}-] - {1}'.format(colored('TIME', 'magenta'),
-              'Execution time -> {0}'.format(msg)))
+              'Execution time -> {0}'.format(msg)), end=end)
